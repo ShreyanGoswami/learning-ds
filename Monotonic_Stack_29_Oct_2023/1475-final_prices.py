415ae6c9eaad5cd3d28cd6f24902418db6bd1527
Jorghy-Del <91205677+Jorghy-Del@users.noreply.github.com>
2023-12-08 11:55:05 +0530

    edit 1475

--- Monotonic_Stack_29_Oct_2023/1475-final_prices.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def finalPrices(self, prices: List[int]) -> List[int]:
-        s = []
-        for i, p in enumerate(prices):
-            while s and prices[s[-1]] >= p:
-                j = s.pop()
-                prices[j] -= p
-            s.append(i)
-        return prices 
